628cb4733e61264d92fda8a26e13af145bc6c916
Emma Hintsala <emmahintsala@kamk.fi>
2024-10-21 14:40:29 +0300

    3.6

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ app.post('/api/persons', (request, response) => {
         error: 'name or number is missing' 
       })
     }
+
+    const nameExists = persons.some(person => person.name === body.name)
+    if (nameExists) {
+      return response.status(400).json({ 
+        error: 'name must be unique' 
+      })
+    }
+    
     const person = {
         id: generateId().toString(),
         name: body.name,
